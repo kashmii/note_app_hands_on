c35634219eff4055e51c2278b85a2189a581d6f1
kashmii <www.myac@gmail.com>
2023-11-27 12:45:49 +0900

    ノートの詳細・編集・削除ページの作成

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,5 +1,6 @@
+import { zUpsertNote } from "@/app/notes/type";
 import { prisma } from "@/globals/db";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // 1. 動的レンダリングを強制する
 // このAPIを使用するpagesやlayoutsは動的にレンダリングされるようになる
@@ -10,4 +11,13 @@ export async function GET() {
   // 3. DBからノート一覧を取得
   const notes = await prisma.note.findMany();
   return NextResponse.json(notes)
+}
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const parcedData = zUpsertNote.parse(data);
+  const note = await prisma.note.create({
+    data: { title: parcedData.title, body: parcedData.body },
+  });
+  return new NextResponse(`${note.id}`, { status: 201 })
 }
